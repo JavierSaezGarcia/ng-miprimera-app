9a63246acb3240979790637a3cf05b93b8c58657
Javier Saez Garcia <banbleac@gmail.com>
2022-10-10 16:41:04 +0200

    Primer-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+// decorador que lleva un selector,una plantilla , un estilo y una clase
 @Component({
-  selector: 'app-root',
+  selector: 'app-raiz',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'miprimeraApp';
+  saludo = "Hola amiguitos";
 }
---
+++ src/app/empleados/empleados.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+// Creamos el decorador
+@Component({
+    selector: 'app-empleados',
+    templateUrl: './empleados.component.html',
+    styleUrls: ['./empleados.component.css']
+})
+
+// construimos la clase para que sea accesible desde fuera desde este archivo
+export class EmpleadosComponent{
+
+
+}
